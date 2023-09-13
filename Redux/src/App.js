85fa7bd4ea209634fe50d0f01cea0e0d726807bc
Solonchenko Ivan <vanek.solonchenko@gmail.com>
2023-09-13 22:10:25 +0300

    Redux thunk and asynchronous actions

--- Redux/src/App.js
+++ Redux/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import './App.css';
 import {useDispatch, useSelector} from "react-redux";
 import {addCustomerAction, removeCustomerAction} from "./store/customerReducer";
+import {fetchCustomers} from "./asyncAction/customers";
 
 function App() {
     const dispatch = useDispatch()
@@ -35,12 +36,12 @@ function App() {
             <button onClick={() => addCash(Number(prompt()))}>Поповнити рахунок</button>
             <button onClick={() => getCash(Number(prompt()))}>Зняти кошти</button>
             <button onClick={() => addCustomer(prompt())}>Додати клієнта</button>
-            <button onClick={() => getCash(Number(prompt()))}>Видалити клієнта</button>
+            <button onClick={() => dispatch(fetchCustomers())}>Отримати клієнтів з бази</button>
         </div>
         {customers.length > 0 ?
             <div>
                 {customers.map(customer =>
-                    <div onClick={() => removeCustomer(customer)} style={{fontSize: "20rem", border: "1px solid black", padding: "10px", marginTop: "5px"}}>{customer.name}</div>
+                    <div onClick={() => removeCustomer(customer)} style={{fontSize: "1rem", border: "1px solid black", padding: "10px", marginTop: "5px"}}>{customer.name}</div>
                 )}
             </div>
             :
